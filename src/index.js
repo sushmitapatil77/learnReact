fcf770d533c6329c112f29a99ce61454946a8cf9
sushmitapatil77 <47168292+sushmitapatil77@users.noreply.github.com>
2020-03-26 16:37:55 -0600

    Added name and lucky number

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "Sush";
+const lname = "P";
+const luckyNum = 7;
 ReactDOM.render(
   <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    {/* string interpreter {`${fname} ${lname}`}*/}
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your lucky number is {luckyNum}</p>
   </div>,
   document.getElementById("root")
 );
